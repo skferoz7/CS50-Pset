781340e4ec95b04c2753663737821217388c7323
Shaik Feroz <146053711+skferoz7@users.noreply.github.com>
2025-10-08 14:19:58 +0530

    Add files via upload

---
+++ do.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<cs50.h>
+int main(void)
+{
+    int n;
+    int i=0;
+   do{
+        int n=get_int("number \n");
+    }
+    while(i<n){
+        i++;
+        printf("meow\n");
+    }
+}
